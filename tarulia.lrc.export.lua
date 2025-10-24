588609c44ed0dfb44181d82baec163e81f12b22c
Tarulia <mihawk.90+git@googlemail.com>
2025-10-24 05:17:02 +0200

    ex: Implement eLRC export

--- tarulia.lrc.export.lua
+++ tarulia.lrc.export.lua
@@ -6,6 +6,8 @@ script_author = "ema"
 script_version = "1"
 ---@diagnostic enable: lowercase-global
 
+---@diagnostic disable-next-line - "Undefined global `include`.(You can treat `include` as `require` by setting.)" no idea what setting
+include("karaskel.lua")
 
 --
 --	Utility functions
@@ -51,6 +53,41 @@ local function to_timecode(time_ms)
 	aegisub.log(4,'timesec: %s | h: %s | m: %s | s: %s | ms: %s\n', time_sec, h, m, s, ms)
 	m = m + h * 60
 	return string.format('%02d:%02d.%02d', m, s, ms)
+end
+--dump an entire table withour values<br>
+---necessary for big tables that run into a stack overflow<br>
+---https://stackoverflow.com/a/27028488/3323286
+---@param o table
+---@return string
+local function dump(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. ','-- .. dump(v)
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
+end
+
+---dump an entire table with values<br>
+---necessary for big tables that run into a stack overflow<br>
+---https://stackoverflow.com/a/27028488/3323286
+---@param o table
+---@return string
+local function dump_full(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. dump_full(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
 end
 
 --
@@ -114,6 +151,8 @@ local function ass_to_lrc(subs, sel)
 
 	---@diagnostic disable-next-line: need-check-nil -- canceled in start_lyrics_file
 	output_file:close()
+
+	aegisub.log(3, 'LRC Export finished')
 end
 
 ---Macro: eLRC<br>
@@ -124,16 +163,50 @@ end
 local function ass_to_elrc(subs, sel)
 	local output_file = start_lyrics_file(".elrc")
 
-	-- for i = 1, #subs, 1 do
-	-- 	local line = subs[i]
-	-- 	if line.class == 'dialogue' and line.comment == false then
-	-- 		---@diagnostic disable-next-line: need-check-nil -- canceled in start_lyrics_file
-	-- 		output_file:write(to_lrc_line(line.start_time, strip_tags(line.text)))
-	-- 	end
-	-- end
+	local meta, styles = karaskel.collect_head(subs)
+
+	for lineCounter = 1, #subs, 1 do
+		local line = subs[lineCounter]
+
+		--aegisub.log(3, 'type(line): %s\n', type(line))
+		if line.class == 'dialogue' and line.comment == false then
+			aegisub.log(5, 'BEFORE preproc_line_text\n')
+			aegisub.log(5, 'dump_full(line):\n%s\n\n', dump_full(line))
+			karaskel.preproc_line_text(meta, styles, line)
+			aegisub.log(5, 'AFTER preproc_line_text\n')
+			aegisub.log(5, 'dump_full(line):\n%s\n\n', dump_full(line))
+			aegisub.log(5, 'dump_full(line.kara):\n%s\n\n', dump_full(line.kara))
+
+			local elrcLine = ''
+			local syl
+			for sylCounter = 1, #line.kara, 1 do
+				syl = line.kara[sylCounter]
+				aegisub.log(5, '\nline.start_time: %s\n', line.start_time)
+				aegisub.log(5, 'sylCounter: %s\n', sylCounter)
+				aegisub.log(5, 'dump(syl):\n%s\n', dump(syl))
+				aegisub.log(5, 'dump_full(syl.duration): %s\n', dump_full(syl.duration))
+				aegisub.log(5, 'dump_full(syl.kdur): %s\n', dump_full(syl.kdur))
+				aegisub.log(5, 'dump_full(syl.start_time): %s\n', dump_full(syl.start_time))
+				aegisub.log(5, 'dump_full(syl.end_time): %s\n', dump_full(syl.end_time))
+				aegisub.log(5, 'dump_full(syl.text): %s\n', dump_full(syl.text))
+				elrcLine = string.format('%s<%s>%s<%s>',
+							elrcLine,
+							to_timecode(line.start_time + syl.start_time),
+							syl.text,
+							to_timecode(line.start_time + syl.end_time)
+						   )
+			end
+			aegisub.log(4, 'elrcLine: %s\n', elrcLine)
+
+			---@diagnostic disable-next-line: need-check-nil -- canceled in start_lyrics_file
+			output_file:write(to_lrc_line(line.start_time, elrcLine))
+		end
+	end
 
 	---@diagnostic disable-next-line: need-check-nil -- canceled in start_lyrics_file
 	output_file:close()
+
+	aegisub.log(3, 'eLRC Export finished')
 end
 
 aegisub.register_macro(script_name .. ' - LRC', script_description, ass_to_lrc)
